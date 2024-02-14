287e0e03682c3fec6231d5da58f355d65b4c1c32
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2024-02-14 11:55:53 +0530

    fix

--- components/downloader.js
+++ components/downloader.js
@@ -1,13 +1,14 @@
 const {clash} = require("../lib/");
 const fetch = require("node-fetch");
+const axios = require("axios");
 
 clash({pattern: "insta", fromMe: false, desc: "Download posts from Instagram", type: "downloader",},
 async ({args, msg, conn}) => {
 if(!args) return await msg.tinyreply("*_Enter instagram post url!_*");
 let {key} = await msg.tinyreply("*_Downloading..._*");
 try{
-var res = await fetch(`https://api-viper-x.koyeb.app/api/insta?url=${args}`)
-let response = await res.json();
+var res = await axios.get(`https://api-viper-x.koyeb.app/api/insta?url=${args}`)
+let response = await res.data
 for (let i of response.data) {
 var type = i.type
 var media = await (await fetch(`${i.url}`)).buffer()
@@ -15,6 +16,7 @@ await conn.sendMessage(msg.from,{[type]:{url:media}},{quoted:msg});
 return await msg.editmsg("*_Uploaded!_*", key);
 }
 } catch (e) {
+console.log(e);
 msg.editmsg("*_Error!_*", key);
 }
 });
