c43cc45b9b9832443b0306cc182e4e4599959cc2
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2024-02-14 11:43:59 +0530

    fixed?

--- components/downloader.js
+++ components/downloader.js
@@ -1,16 +1,17 @@
-const {clash, API} = require("../lib/");
-const axios = require("axios");
+const {clash} = require("../lib/");
+const fetch = require("node-fetch");
 
 clash({pattern: "insta", fromMe: false, desc: "Download posts from Instagram", type: "downloader",},
 async ({args, msg, conn}) => {
 if(!args) return await msg.tinyreply("*_Enter instagram post url!_*");
 let {key} = await msg.tinyreply("*_Downloading..._*");
 try{
-var res = await axios.get(`${API}/api/insta?url=${args}`)
-let response = await res.data
-for (let i of response.data) {
+var res = await fetch(`https://toxic-kichux-aswin-sparky.koyeb.app/api/instagram?url=${args}`)
+let response = await res.json();
+for (let i of response.result.data) {
 var type = i.type
-await conn.sendMessage(msg.from,{[type]:{url:i.url}},{quoted:msg});
+var media = await (await fetch(`${i.url}`)).buffer()
+await conn.sendMessage(msg.from,{[type]:{url:media}},{quoted:msg});
 return await msg.editmsg("*_Uploaded!_*", key);
 }
 } catch (e) {
