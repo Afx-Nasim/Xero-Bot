3c2bbd2af0a05d6d057009ad14f5f97cfdb19024
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-02-14 10:08:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ module.exports = {
   HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^,',
   BRANCH: "master",
   OWNER_NUMBER: ["919961857267@s.whatsapp.net", "0@s.whatsapp.net"],
-  STICKER_DATA: process.env.STICKER_DATA || "CLASH-WA-BOT;TOXIC-KICHUX",
+  STICKER_DATA: process.env.STICKER_DATA || "XERO-Mad;Afx-Nazim",
   SUDO: process.env.SUDO || "919961857267,0",
   API: process.env.API || "https://api-viper.onrender.com", //don't change this link. If you change then some commands will not work!.
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-  OWNER_NAME: process.env.OWNER_NAME || "TOXIC-KICHU",
-  BOT_NAME: process.env.BOT_NAME || "CLASH-WA-BOT",
+  OWNER_NAME: process.env.OWNER_NAME || "Afx-Nasim",
+  BOT_NAME: process.env.BOT_NAME || "XERO-MD",
   WORK_TYPE: process.env.WORK_TYPE || "public",
   DATABASE_URL: DATABASE_URL,
   DATABASE:
