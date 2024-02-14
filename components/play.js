af3e363350b8e848b671530d0807d3b0c3874a9d
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-02-14 13:05:52 +0530

    Create play.js

---
+++ components/play.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+const axios = require("axios");
+async function play() {
+let res = await axios.get(`https://toxic-kichux-aswin-sparky.koyeb.app/api/play?text=Dusk+till-Dwan+Slowed`);
+let result = await res.data
+const aud = await (await fetch(`${result.result.download_url}`)).buffer();
+const thumb = await (await fetch(`${result.result.thumbnail}`)).buffer();
+await conn.sendMessage(msg.from,{image: thumb, caption:`_*Downloading ${result.result.title}*_`},{quoted:msg});
+return await conn.sendMessage(msg.from,{audio: aud, mimetype : 'audio/mpeg'} , { quoted : msg})
+}
+play()
