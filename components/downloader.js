5131c0e89c9a3775561f8bebce286e64d1145323
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2024-02-14 11:51:31 +0530

    fixed

--- components/downloader.js
+++ components/downloader.js
@@ -6,9 +6,9 @@ async ({args, msg, conn}) => {
 if(!args) return await msg.tinyreply("*_Enter instagram post url!_*");
 let {key} = await msg.tinyreply("*_Downloading..._*");
 try{
-var res = await fetch(`https://toxic-kichux-aswin-sparky.koyeb.app/api/instagram?url=${args}`)
+var res = await fetch(`https://api-viper-x.koyeb.app/api/insta?url=${args}`)
 let response = await res.json();
-for (let i of response.result.data) {
+for (let i of response.data) {
 var type = i.type
 var media = await (await fetch(`${i.url}`)).buffer()
 await conn.sendMessage(msg.from,{[type]:{url:media}},{quoted:msg});
