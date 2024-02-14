36fe1900fb880ffa35f3c7caf2e7166997909404
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-02-14 16:08:44 +0530

    Update play.js

--- components/play.js
+++ components/play.js
@@ -9,8 +9,8 @@ let res = await axios.get(`https://toxic-kichux-aswin-sparky.koyeb.app/api/play?
 let result = await res.data
 const aud = await (await fetch(`${result.result.download_url}`)).buffer();
 const thumb = await (await fetch(`${result.result.thumbnail}`)).buffer();
-//await conn.sendMessage(msg.from,{image: thumb, caption:`_*Downloading ${result.result.title}*_`},{quoted:msg});
+await conn.sendMessage(msg.from,{image: thumb, caption:`_*Downloading ${result.result.title}*_`},{quoted:msg});
 return await conn.sendMessage(msg.from,{audio: aud, mimetype : 'audio/mpeg'} , { quoted : msg})
 }
-play()//
+play()
 });
