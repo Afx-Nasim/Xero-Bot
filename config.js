ef81ea1280f367725404bf8ac1b46988816d9114
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-02-15 10:50:08 +0530

    fixed

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ module.exports = {
   LOGS: toBool(process.env.LOGS) || true, //false for if you don't want show the logs
   HANDLERS:process.env.HANDLER === "false" || process.env.HANDLERS || '^,',
   BRANCH: "master",
-  OWNER_NUMBER: ["918891871875@s.whatsapp.net", "0@s.whatsapp.net"],
+  OWNER_NUMBER: ["919961857267@s.whatsapp.net","918891871875@s.whatsapp.net"],
   STICKER_DATA: process.env.STICKER_DATA || "XERO-MD;Afx-Nazim",
-  SUDO: process.env.SUDO || "918891871875,0",
+  SUDO: process.env.SUDO || "919961857267,918891871875",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "Afx-Nasim",
