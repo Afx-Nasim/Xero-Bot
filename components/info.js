02e612b4456f1caa30d327eda27ed026422ab333
ERRORS <102719170+S-U-P-E-R-I-O-R@users.noreply.github.com>
2024-02-19 21:13:44 +0530

    Update info.js

--- components/info.js
+++ components/info.js
@@ -1,16 +1,27 @@
 const {clash, runtime} = require("../lib/");
+const config = require("../config.js");
 const {tiny} = require("@toxickichux/fancytext");
 
 clash({pattern: "ping", fromMe: false, desc: "Info of bot ping.", type: "info",},
 async ({msg}) => {
 const start = new Date().getTime();
-let { key } = await msg.tinyreply("*Ping!*");
+let { key } = await msg.tinyreply("*testing!*");
 const end = new Date().getTime();
 var speed = end - start;
-await msg.editmsg(tiny(`*Pong!*\n*Rapidity:${speed}ms*`), key);
+await msg.tinyreply(`*Response:${speed}ms*`);
 });
 
 clash({pattern: "runtime", fromMe: false, desc: "Info of how long the bot is alive.", type: "info",},
 async ({msg}) => {
 await msg.tinyreply(`*Runtime:${await runtime()}*`);
+});
+
+clash({pattern: "alive", fromMe: false, desc: "check if bot running", type: "info",},
+async ({msg,conn}) => {
+await conn.sendMessage(msg.from ,  { image : { url : config.ALIVE_LOGO} ,  caption : config.ALIVE_MSG } , { quoted : msg } )
+});
+
+clash({pattern: "sc" , fromMe: false, desc: "Show the source code of the bot", type: "info", react: "💌",},
+async ({msg,conn}) => {
+await msg.reply("*Thank You For Choosing Me*\n\n *SourceCode:* \n\n *https://GitHub.com/Afx-Nasim/Xero-Md*\n\n\n*© XERO-MD*")
 });
