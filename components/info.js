fbc6ae90eb141a31e7d9467d3e9314c30aca5306
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-03-08 16:43:08 +0530

    Update info.js

--- components/info.js
+++ components/info.js
@@ -4,11 +4,12 @@ const { tiny } = require("@toxickichux/fancytext");
 clash({ pattern: "ping", fromMe: false, desc: "Info of bot ping.", type: "info" },
   async ({ msg }) => {
     const start = new Date().getTime();
-    let message = await msg.tinyreply("*Ping!*");
+    const message = await msg.tinyreply("*Ping!*");
     const end = new Date().getTime();
-    var speed = end - start;
-    await msg.tinyedit(tiny(`*Rapidity: ${speed}ms*`), message);
+    const speed = end - start;
+    await msg.editmsg(tiny(`*Rapidity: ${speed}ms*`), message.key);
   });
+
 
 clash({ pattern: "runtime", fromMe: false, desc: "Info of how long the bot is alive.", type: "info" },
   async ({ msg }) => {
