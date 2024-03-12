ec5326dc617d4f0f9087a44ef6a9156a5c40f2ff
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2024-03-12 16:08:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ module.exports = {
   OWNER_NAME: process.env.OWNER_NAME || "Afx-Nasim",
   BOT_NAME: process.env.BOT_NAME || "XERO-MD",
   THUMB_NAIL: process.env.THUMB_NAIL || "https://i.imgur.com/KY03JjV.jpeg",
+  ABHI_API: process.env.ABHI_API || "https://abhi-api-bvws.onrender.com/",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hey {sender}, I'm alive \n Uptime: {uptime}",
   ALIVE_LOGO: process.env. ALIVE_LOGO || "https://i.imgur.com/KY03JjV.jpeg",
   WORK_TYPE: process.env.WORK_TYPE || "public",
