62532b6122a1ed65fe39fa41fe4e9d14e14e2827
Afx-Nasim <112264992+Afx-Nasim@users.noreply.github.com>
2024-03-16 20:02:32 +0530

    fix

--- components/info.js
+++ components/info.js
@@ -1,17 +1,1 @@
-const { clash, runtime } = require("../lib/");
-const { tiny } = require("@toxickichux/fancytext");
-
-clash({ pattern: "ping", fromMe: false, desc: "Info of bot ping.", type: "info" },
-  async ({ msg }) => {
-    const start = new Date().getTime();
-    const message = await msg.tinyreply("*Ping!*");
-    const end = new Date().getTime();
-    const speed = end - start;
-    await msg.editmsg(tiny(`*Rapidity: ${speed}ms*`), message.key);
-  });
-
-
-clash({ pattern: "runtime", fromMe: false, desc: "Info of how long the bot is alive.", type: "info" },
-  async ({ msg }) => {
-    await msg.tinyreply(`*Runtime: ${await runtime()}*`);
-  });
+const _0x3e4de9=_0x5ae5;function _0x5ae5(_0x1a3f0e,_0x286856){const _0x3ebce8=_0x3ebc();_0x5ae5=function(_0x5ae5ba,_0x50af43){_0x5ae5ba=_0x5ae5ba-0x18e;let _0x58048e=_0x3ebce8[_0x5ae5ba];return _0x58048e;};return _0x5ae5(_0x1a3f0e,_0x286856);}(function(_0x480d40,_0x4c1e65){const _0x3391ec=_0x5ae5;const _0x246f62=_0x480d40();while(!![]){try{const _0x84b77=-parseInt(_0x3391ec(0x191))/0x1+parseInt(_0x3391ec(0x19f))/0x2+-parseInt(_0x3391ec(0x196))/0x3*(-parseInt(_0x3391ec(0x19c))/0x4)+parseInt(_0x3391ec(0x19e))/0x5+parseInt(_0x3391ec(0x19b))/0x6+-parseInt(_0x3391ec(0x18e))/0x7*(-parseInt(_0x3391ec(0x194))/0x8)+-parseInt(_0x3391ec(0x18f))/0x9;if(_0x84b77===_0x4c1e65){break;}else{_0x246f62['push'](_0x246f62['shift']());}}catch(_0xf8891b){_0x246f62['push'](_0x246f62['shift']());}}}(_0x3ebc,0xc9d60));const {clash,runtime}=require('../lib/');const {tiny}=require('@toxickichux/fancytext');clash({'pattern':'ping','fromMe':![],'desc':'Info\x20of\x20bot\x20ping.','type':_0x3e4de9(0x19d)},async({msg:_0x9000d0})=>{const _0x5dfc4d=_0x3e4de9;const _0x2f652e=new Date()['getTime']();const _0x6d4a64=await _0x9000d0[_0x5dfc4d(0x198)](_0x5dfc4d(0x190));const _0x414df0=new Date()['getTime']();const _0x5abcae=_0x414df0-_0x2f652e;await _0x9000d0[_0x5dfc4d(0x199)](tiny(_0x5dfc4d(0x193)+_0x5abcae+_0x5dfc4d(0x192)),_0x6d4a64[_0x5dfc4d(0x197)]);});clash({'pattern':_0x3e4de9(0x19a),'fromMe':![],'desc':_0x3e4de9(0x195),'type':_0x3e4de9(0x19d)},async({msg:_0x15756a})=>{const _0x57286b=_0x3e4de9;await _0x15756a[_0x57286b(0x198)](_0x57286b(0x1a0)+await runtime()+'*');});function _0x3ebc(){const _0x52dbbb=['488310ZJFhRJ','ms*','*Rapidity:\x20','97936jCxXGr','Info\x20of\x20how\x20long\x20the\x20bot\x20is\x20alive.','291303zcpeGg','key','tinyreply','editmsg','runtime','4510284bMDvqn','36GQUZyN','info','1384370jakYmE','1748704RJVnoy','*Runtime:\x20','266IQgfNr','17343135lWOEmc','*Ping!*'];_0x3ebc=function(){return _0x52dbbb;};return _0x3ebc();}
